03242bb4c9c65af054724da3763e808ab45a2f2d
Bruno Martins <bgcngm@gmail.com>
2025-11-12 00:02:20 +0530

    fogos: Set Lineage Health control charging path
    This must now be set per device after migrating to soong_config_set.
    
    Change-Id: I5dd2801391b0e790bafa81bde81fe2d7b737fb30
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- device.mk
+++ device.mk
@@ -52,6 +52,9 @@ PRODUCT_PACKAGES += \
     init.mmi.overlay.rc \
     init.oem.fingerprint2.sh
 
+# Lineage Health
+$(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/power_supply/battery/input_current_limit)
+
 # LiveDisplay
 $(call soong_config_set,livedisplay_sysfs,enable_ab,true)
 
